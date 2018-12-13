5564765d1b36f75a99828c71baa36e978e532058
Ryan Mccampbell <rmccampbell7@gmail.com>
2018-12-13 16:05:28 -0500

    updated run_traces

--- run_traces.sh
+++ run_traces.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 if [[ "$#" -eq 0 || "$1" =~ ^(-h|--help)$ ]]; then
-	echo "usage: run_traces.sh <out_dir> [-u num-uops-to-simulate]" >&2
-	exit 1
+    echo "usage: run_traces.sh <out_dir> [-u num-uops-to-simulate]" >&2
+    exit 1
 fi
 
 dir="$(dirname "${BASH_SOURCE[0]}")"
 outdir="$1"
 
 mkdir -p "$outdir"
+i=1
 for f in "$dir/traces/"*.bz2; do
-    echo "$f"
-	./cbp3 -t "$f" "${@:2}" | tee "$outdir/$(basename $f .bz2).out"
-	echo
-	echo
+    echo "$i: $f"
+    ((i++))
+    ./cbp3 -t "$f" "${@:2}" | tee "$outdir/$(basename $f .bz2).out"
+    echo
+    echo
 done
